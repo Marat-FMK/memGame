ec937c0c250f04babcdd13f4611d60b04df695d0
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-19 02:00:47 +0300

    VM

--- memGame/QuestionViewModel.swift
+++ memGame/QuestionViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by Marat Fakhrizhanov on 18.12.2024.
 //
 
-import Foundation
+
+import SwiftUI
+@Observable
+class QuestionViewModel: ObservableObject {
+    
+    let networkManager = NetworkManager()
+    
+    
+    var mems: [Mem] = []
+    
+    func getMems() {
+//        let mems: AllMemes?
+        
+        networkManager.fetchMems { result in
+            switch result {
+                
+            case .success(let result):
+                
+                if let mems = result.data?.memes {
+                    self.mems = mems
+                }
+                
+            case .failure( let error):
+               print(error)
+            }
+        }
+    }
+    
+    
+}
+
+
