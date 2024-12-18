c23104809bf21fd16d8c819f844b8353793e8b1b
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-19 02:00:22 +0300

    model

--- memGame/MemModel.swift
+++ memGame/MemModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct AllMemes: Decodable {
+    let data: DataMemes?
+}
+
+struct DataMemes : Decodable {
+    let memes: [Mem]?
+}
+
+struct Mem: Decodable {
+    let id: String?
+    let name: String?
+    let url: String?
+}
