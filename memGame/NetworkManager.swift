b1642f791b9de9f734fd1d32e27c7c260e136d47
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-19 02:00:33 +0300

    manager

--- memGame/NetworkManager.swift
+++ memGame/NetworkManager.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+enum ErrorList: Error {
+    case invalidURL
+    case noData
+    case decodError
+    case reguestError
+}
+
+class NetworkManager {
+    let link = "https://api.imgflip.com/get_memes"
+    
+    func fetchMems(completion: @escaping (Result<AllMemes, Error>) -> Void) {
+        
+        guard let url = URL(string: link) else {
+            completion(.failure(ErrorList.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _ , error in
+            
+            guard error == nil else {
+                completion(.failure(error!))
+                return }
+            
+            guard let data else {
+                completion(.failure(ErrorList.noData))
+                return }
+            
+            if let responce = try? JSONDecoder() .decode (AllMemes.self, from: data) {
+                completion(.success(responce))
+            } else {
+                completion(.failure(ErrorList.decodError))
+            }
+        }
+        .resume()
+    }
+}
+       
+    
+
