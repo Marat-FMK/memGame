8101870735cf21243e1668def1d920f3292e6285
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-18 23:31:48 +0300

    Initial Commit

---
+++ memGame/memGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memGameApp.swift
+//  memGame
+//
+//  Created by Marat Fakhrizhanov on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct memGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
